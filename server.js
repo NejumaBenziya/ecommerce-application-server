aaad3ed3a9f53f405e8055d1c1039e063a7d86b5
NejumaBenziya <sulfinejuma@gmail.com>
2025-07-25 13:06:21 +0530

    get command

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express=require("express")
 const app=express()
 const port=3000
+app.get("/",(req,res)=>{
+    res.send("Ecommerce App")
+})
 app.listen(port,()=>{
     console.log("server running...")
 })
