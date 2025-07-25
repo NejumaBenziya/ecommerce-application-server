faf05e094905f1e1b8c9d1c989a50a05afafb617
NejumaBenziya <sulfinejuma@gmail.com>
2025-07-25 13:17:56 +0530

    erorr

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express=require("express")
 const app=express()
 const port=3000
-app.get("/",(req,res)=>{
-    res.send("Ecommerce App")
+app.get('/',(req,res)=>{
+    res.send('Ecommerce App')
+    console.log("error");
+    
 })
 app.listen(port,()=>{
     console.log("server running...")
